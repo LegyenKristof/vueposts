cf16e1f213bee446b786fa7199649c2e2fefb183
LegyenKristof <48032365+LegyenKristof@users.noreply.github.com>
2021-12-01 23:41:02 +0100

    Blog component done

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Blog :bejegyzesek="posts"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Blog from "./components/Blog.vue"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Blog
   },
   data() {
     return {
---
+++ src/components/Blog.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+      <Bejegyzes v-for="bejegyzes in bejegyzesek" :key="bejegyzes.title" />
+  </div>
+</template>
+
+<script>
+import Bejegyzes from "./Bejegyzes.vue"
+
+export default {
+    components: {
+        Bejegyzes
+    },
+    props: ["bejegyzesek"]
+}
+</script>
